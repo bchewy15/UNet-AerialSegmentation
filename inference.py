c5c981236359aed3e2da8663a4633535b376a5c6
Ben <benchui15@gmail.com>
2024-03-01 14:16:15 -0400

    Let the user pic a file to inference.

--- inference.py
+++ inference.py
@@ -4,6 +4,7 @@ from PIL import Image
 from model import UNet
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 transform = transforms.Compose([
@@ -26,9 +27,31 @@ def predict_single_image(image_path, model):
 
 # take one pic and inference, could make this take in a user selection
 if __name__ == '__main__':
-    image_path = 'images/im4.jpg'
-    predicted_image = predict_single_image(image_path, model)
+    image_files = os.listdir('images/')
+    for i, file in enumerate(image_files):
+        print(f"{i+1}. {file}")
+
+    # choose an image
+    while True:
+        try:
+            choice = int(input("Choose an image by typing its number: "))
+            if 1 <= choice <= len(image_files):
+                break
+            else:
+                print("Invalid choice. Please choose a valid number.")
+        except ValueError:
+            print("Invalid choice. Please choose a valid number.")
+
+    #  get the image and inference
+    chosen_image = os.path.join('images', image_files[choice - 1])
+    predicted_image = predict_single_image(chosen_image, model)
+
+    # display
     plt.imshow(predicted_image)
     plt.axis('off')
-    plt.savefig('images/predicted_mask.png')
+
+    # save the prediction
+    predict_name = os.path.splitext(image_files[choice - 1])[0] + '-prediction.png'
+    predict_path = os.path.join('predictions', predict_name)
+    plt.savefig(predict_path)
     plt.show()
